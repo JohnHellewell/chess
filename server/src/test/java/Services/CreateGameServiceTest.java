1c0cb1f0614c67f7d6aa4f4abedf47a5293a7728
JohnHellewell <john.hellewell123@gmail.com>
2024-02-27 20:42:54 -0700

    added test for create game

---
+++ server/src/test/java/Services/CreateGameServiceTest.java
@@ -0,0 +1,27 @@
+package Services;
+
+import Requests.CreateGameRequest;
+import dataAccess.DataAccess;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateGameServiceTest {
+    @Test
+    public void createGameTest(){
+        DataAccess.clearAll();
+        String authToken = DataAccess.addUser("a", "b", "c");
+
+        try {
+            CreateGameRequest req = new CreateGameRequest();
+            req.gameName = "hello";
+            Assertions.assertEquals(401, (new CreateGameService()).createGame("banana", req).getCode());
+
+            Assertions.assertEquals(200, (new CreateGameService()).createGame(authToken, req).getCode());
+        } catch(Exception e){
+            Assertions.fail();
+        }
+    }
+
+}
