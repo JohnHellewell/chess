dd7810d25988f3ab43ca4af2ecc6161dfedbd0ed
JohnHellewell <john.hellewell123@gmail.com>
2024-02-27 20:30:38 -0700

    clearservice test

---
+++ server/src/test/java/Services/ClearServiceTest.java
@@ -0,0 +1,21 @@
+package Services;
+
+import dataAccess.DataAccess;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClearServiceTest {
+    @Test
+    public void clearTest(){
+        DataAccess.addUser("a", "b", "c");
+        Assertions.assertEquals("a", DataAccess.getUser("a").getUsername());
+
+        ClearService cs = new ClearService();
+        cs.clearAll();
+        Assertions.assertNull(DataAccess.getUser("a"));
+
+    }
+
+}
