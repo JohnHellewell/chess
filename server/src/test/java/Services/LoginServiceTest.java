5b91672e318787ee544f5e9d16081fbfe370467a
JohnHellewell <john.hellewell123@gmail.com>
2024-02-27 21:14:08 -0700

    added logintest

---
+++ server/src/test/java/Services/LoginServiceTest.java
@@ -0,0 +1,27 @@
+package Services;
+
+import Requests.LoginRequest;
+import dataAccess.DataAccess;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginServiceTest {
+    @Test
+    public void LoginTest(){
+        DataAccess.clearAll();
+        LoginRequest temp = new LoginRequest();
+        temp.password = "b";
+        temp.username = "a";
+        try {
+            Assertions.assertEquals(401, (new LoginService()).login(temp).getCode());
+
+            DataAccess.addUser("a", "b", "c");
+            Assertions.assertEquals(200, (new LoginService()).login(temp).getCode());
+        } catch(Exception e){
+            Assertions.fail();
+        }
+    }
+
+}
