5cf83cfd0db1643eadc468e61c1880218c97775d
JohnHellewell <john.hellewell123@gmail.com>
2024-02-27 21:24:10 -0700

    added registration test

---
+++ server/src/test/java/Services/RegistrationServiceTest.java
@@ -0,0 +1,27 @@
+package Services;
+
+import Handlers.RegistrationHandler;
+import Requests.RegistrationRequest;
+import dataAccess.DataAccess;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceTest {
+    @Test
+    public void RegisterTest(){
+        DataAccess.clearAll();
+        RegistrationRequest req = new RegistrationRequest();
+        req.username = "a";
+        req.password = "b";
+        req.email = "c.gmail.com";
+        try{
+            Assertions.assertEquals(200, (new RegistrationService()).registerUser(req).getCode());
+            Assertions.assertEquals(403, (new RegistrationService()).registerUser(req).getCode());
+
+        } catch(Exception e){
+            Assertions.fail();
+        }
+    }
+}
