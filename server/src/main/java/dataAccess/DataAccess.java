aa4d9494bda84e65f998ae93b3ffd5e105d4ecbd
JohnHellewell <john.hellewell123@gmail.com>
2024-03-06 13:49:23 -0700

    added some sql code

--- server/src/main/java/dataAccess/DataAccess.java
+++ server/src/main/java/dataAccess/DataAccess.java
@@ -11,18 +11,56 @@ public class DataAccess {
     private static ArrayList<GameData> gameData = new ArrayList<GameData>();
     private static ArrayList<AuthData> authData = new ArrayList<AuthData>();
     public DataAccess(){
-        userData = new ArrayList<UserData>();
-        gameData = new ArrayList<GameData>();
-        authData = new ArrayList<AuthData>();
+        try {
+            DatabaseManager.createDatabase();
+        } catch(Exception e){
+            //nothing..?
+        }
     };
 
+    private static void runSQL(String code){
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(code)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (Exception e){
 
+        }
+    }
+
+    private static String querySQL(String code){
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(code)) {
+                var rs = preparedStatement.executeQuery();
+                rs.next();
+                return rs.getString(1);
+            }
+        } catch (Exception e){
+                return "";
+        }
+    }
+
+    /*
+    public void example() throws Exception {
+       try (var conn = DatabaseManager.getConnection()) {
+          try (var preparedStatement = conn.prepareStatement("SELECT 1+1")) {
+             var rs = preparedStatement.executeQuery();
+             rs.next();
+             System.out.println(rs.getInt(1));
+          }
+       }
+    }
+     */
 
     public static void clearAll(){
-        userData.clear();
-        gameData.clear();
-        authData.clear();
+        runSQL("DELETE FROM userdata");
+
+        //userData.clear();
+        //gameData.clear();
+        //authData.clear();
     }
+
+
 
     public static UserData getUser(String username){
         for(UserData u : userData){
@@ -39,10 +77,15 @@ public class DataAccess {
     }
 
     public static String addUser(String username, String password, String email){ //returns auth data
+        /*
         userData.add(new UserData(username, password, email));
         String token = generateAuthToken();
         authData.add(new AuthData(token, username));
         return token;
+        */
+        String authToken = querySQL("INSERT into userdata(\"" + username + "\", \"" + password + "\", \"" + email + "\")");
+        return authToken;
+        //not sure how to get string
     }
 
     public static boolean isAuthValid(String authToken){
