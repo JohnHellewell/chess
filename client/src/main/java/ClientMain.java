f29259d10383d6d57c80f16596c8c08c0cce6cc6
JohnHellewell <john.hellewell123@gmail.com>
2024-03-25 15:07:11 -0600

    added logged_in menu

--- client/src/main/java/ClientMain.java
+++ client/src/main/java/ClientMain.java
@@ -4,6 +4,7 @@ public class ClientMain {
     private static boolean loggedIn = false;
 
     private static String authToken = "";
+    private static String username = "";
     public static void main(String[] args) {
         //var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         //System.out.println("♕ 240 Chess Client: " + piece);
@@ -14,7 +15,7 @@ public class ClientMain {
 
     private static void awaitInput(){
         if(loggedIn){
-            System.out.print("[LOGGED_IN] >>> ");
+            System.out.print("[LOGGED_IN] "+ username +" >>> ");
         } else {
             System.out.print("[LOGGED_OUT] >>> ");
         }
@@ -42,13 +43,17 @@ public class ClientMain {
                 System.exit(0);
                 break; //i shouldn't need this line but i included it
             }
-            case "REGISTER": {
-                register(input.split(" "));
-                break;
+            case "REGISTER": { //logged out exclusive
+                if(!loggedIn) {
+                    register(input.split(" "));
+                    break;
+                }
             }
-            case "LOGIN": {
-                login(input.split(" "));
-                break;
+            case "LOGIN": { //logged out exclusive
+                if(!loggedIn) {
+                    login(input.split(" "));
+                    break;
+                }
             }
             case "CLEAR":{
                 ServerFacade.clear();
@@ -79,8 +84,10 @@ public class ClientMain {
             unrecognizedCommand(args);
 
         authToken = ServerFacade.registerUser(args[1], args[2], args[3]);
-        if(!authToken.isEmpty())
+        if(!authToken.isEmpty()) {
             loggedIn = true;
+            username = args[1];
+        }
     }
 
     private static void login(String[] args){
@@ -95,12 +102,15 @@ public class ClientMain {
         System.out.println("Commands:");
         if(!loggedIn) { //Logged OUT menu
             System.out.println("\tregister <USERNAME> <PASSWORD> <EMAIL>\t-create a new account");
-            System.out.println("\tlogin <USERNAME> <PASSWORD>\t\t\t\t-login into an existing account");
+            System.out.println("\tlogin <USERNAME> <PASSWORD>\t-login into an existing account");
         } else { //Logged IN menu
-            System.out.println("\tregister <USERNAME> <PASSWORD> <EMAIL>\t-create a new account");
-            System.out.println("\tlogin <USERNAME> <PASSWORD>\t\t\t\t-login into an existing account");
+            System.out.println("\tcreate <NAME>\t-create a new game");
+            System.out.println("\tjoin <GAME ID> [\"WHITE\"|\"BLACK\"|blank]\t-join a game as white, black, or spectator");
+            System.out.println("\tlist\t-list all games");
+            System.out.println("\tobserve <GAME ID>\t-spectate a game");
+            System.out.println("\tlogout\t-logout");
         }
-        System.out.println("\thelp\t\t\t\t\t\t\t\t\t-display a list of commands");
-        System.out.println("\tquit\t\t\t\t\t\t\t\t\t-close chess program");
+        System.out.println("\thelp\t-display a list of commands");
+        System.out.println("\tquit\t-close chess program");
     }
 }
